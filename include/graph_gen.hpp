686d59004823f683b6eddff27f33e772cd42c909
andrevbastos <andrevbm.288@gmail.com>
2026-03-25 12:35:46 -0300

    feat: diagonal grid gen

--- include/graph_gen.hpp
+++ include/graph_gen.hpp
@@ -5,62 +5,16 @@
 #include <graph/undirected/graph.hpp>
 #include <graph/util/kruskal.hpp>
 
-bool areNodesConnected(common::Graph* maze, int idA, int idB);
+bool areNodesConnected(common::Graph* maze, int idA, int idB) {
+    common::Node* nodeA = maze->getVertex(idA);
+    if (!nodeA) return false;
 
-common::Graph* createObstacleGraph(int obstaclePercentage, int width, int height)
-{
-    srand(static_cast<unsigned>(time(NULL)));
-
-    auto* g = new undirected::Graph();
-    std::vector<common::Node*> objects = {};
-
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            g->newVertex(std::make_tuple(x, y));
+    for (auto* neighbor : nodeA->adj()) {
+        if (neighbor->getId() == idB) {
+            return true;
         }
     }
-
-    const int totalNodes = width * height;
-
-    std::vector<bool> obstacleGrid(totalNodes, false);
-    for (int i = 0; i < totalNodes; ++i) {
-        int roll = rand() % 100;
-        if (roll < 20) {
-            obstacleGrid[i] = true;
-            objects.push_back(g->getVertex(i));
-        }
-    }
-
-    const int startX = 0, startY = 0, endX = width - 1, endY = height - 1;
-    const int startId = startY * width + startX, endId = endY * width + endX;
-
-    obstacleGrid[startId] = false;
-    obstacleGrid[endId] = false;
-
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            int currentId = y * width + x;
-            if (obstacleGrid[currentId]) continue; 
-
-            common::Node* currentNode = g->getVertex(currentId);
-
-            if (x + 1 < width) {
-                int rightId = y * width + (x + 1);
-                if (!obstacleGrid[rightId]) {
-                    g->newEdge(currentNode, g->getVertex(rightId));
-                }
-            }
-
-            if (y + 1 < height) {
-                int bottomId = (y + 1) * width + x;
-                if (!obstacleGrid[bottomId]) {
-                    g->newEdge(currentNode, g->getVertex(bottomId));
-                }
-            }
-        }
-    }
-
-    return g;
+    return false;
 };
 
 common::Graph* createMazeGraph(int height, int width)
@@ -85,15 +39,25 @@ common::Graph* createMazeGraph(int height, int width)
                 g->newEdge(currentNode, g->getVertex(rightId), rand() % 100 + 1);
             }
 
+            if (x + 1 < width && y + 1 < height) {
+                int diagRightId = (y + 1) * width + (x + 1);
+                g->newEdge(currentNode, g->getVertex(diagRightId), rand() % 100 + 1);
+            }
+
             if (y + 1 < height) {
                 int bottomId = (y + 1) * width + x;
                 g->newEdge(currentNode, g->getVertex(bottomId), rand() % 100 + 1);
+            }
+
+            if (x - 1 >= 0 && y + 1 < height) {
+                int diagLeftId = (y + 1) * width + (x - 1);
+                g->newEdge(currentNode, g->getVertex(diagLeftId), rand() % 100 + 1);
             }
         }
     }
 
     auto openPath = util::Kruskal::getMST(g);
-    auto w = g->getWeights();
+    
     for (auto* edge : openPath) {
         result->newEdge(edge->getFirstNode()->getId(), edge->getSecondNode()->getId());
     }
@@ -110,58 +74,56 @@ void exportMazeToTxt(common::Graph* maze, int width, int height, const std::stri
         return;
     }
 
-    for (int x = 0; x < width; ++x) outFile << "+---";
-    outFile << "+" << std::endl;
-
     for (int y = 0; y < height; ++y) {
-        outFile << "|"; 
         
         for (int x = 0; x < width; ++x) {
-            int currentId = y * width + x;
-            outFile << "   ";
+            outFile << "+";
 
             if (x + 1 < width) {
+                int currentId = y * width + x;
                 int rightId = y * width + (x + 1);
+                
                 if (areNodesConnected(maze, currentId, rightId)) {
-                    outFile << " "; 
+                    outFile << "---";
                 } else {
-                    outFile << "|"; 
+                    outFile << "   ";
                 }
-            } else {
-                outFile << "|";
             }
         }
         outFile << std::endl;
 
-        for (int x = 0; x < width; ++x) {
-            outFile << "+";
-            int currentId = y * width + x;
-            
-            if (y + 1 < height) {
+        if (y + 1 < height) {
+            for (int x = 0; x < width; ++x) {
+                int currentId = y * width + x;
                 int bottomId = (y + 1) * width + x;
+
                 if (areNodesConnected(maze, currentId, bottomId)) {
-                    outFile << "   "; 
+                    outFile << "|";
                 } else {
-                    outFile << "---"; 
+                    outFile << " ";
                 }
-            } else {
-                outFile << "---";
+
+                if (x + 1 < width) {
+                    int rightId = y * width + (x + 1);
+                    int bottomRightId = (y + 1) * width + (x + 1);
+                    
+                    bool diag1 = areNodesConnected(maze, currentId, bottomRightId);
+                    bool diag2 = areNodesConnected(maze, rightId, bottomId);
+
+                    if (diag1 && diag2) {
+                        outFile << " X ";
+                    } else if (diag1) {
+                        outFile << " \\ ";
+                    } else if (diag2) {
+                        outFile << " / ";
+                    } else {
+                        outFile << "   ";
+                    }
+                }
             }
+            outFile << std::endl;
         }
-        outFile << "+" << std::endl;
     }
 
     outFile.close();
 }
-
-bool areNodesConnected(common::Graph* maze, int idA, int idB) {
-    common::Node* nodeA = maze->getVertex(idA);
-    if (!nodeA) return false;
-
-    for (auto* neighbor : nodeA->adj()) {
-        if (neighbor->getId() == idB) {
-            return true;
-        }
-    }
-    return false;
-};
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <chrono>
 #include <tuple>
+#include <cmath>
+#include <any>
 #include <graph/util/a_star.hpp>
 #include <graph/util/dijkstra.hpp>
 #include <graph/util/kruskal.hpp>
@@ -52,6 +54,13 @@ int main(int argc, char* argv[]) {
         stats.addEntry("Dijkstra", "Path Length", dijkstra.size());
         stats.addEntry("Dijkstra", "Execution Time", duration.count());
 
+        start = std::chrono::steady_clock::now();
+        auto a_mod = aStarMod(g, startId, endId, util::AStar::chebyshevHeuristic2D);
+        end = std::chrono::steady_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        stats.addEntry("A Star Modified", "Path Length", a_mod.size());
+        stats.addEntry("A Star Modified", "Execution Time", duration.count());
+
         delete g;
     }
 
@@ -84,34 +93,69 @@ std::vector<common::Node*> aStarMod(common::Graph *graph, int startId, int endId
 
     std::unordered_map<common::Node*, common::Node*> VM;
     std::unordered_map<common::Node*, double> CM;
-    std::priority_queue<std::pair<common::Node*, double>, std::vector<std::pair<common::Node*, double>>, std::greater<>> VPQ;
+    
+    std::priority_queue<std::pair<double, common::Node*>, std::vector<std::pair<double, common::Node*>>, std::greater<>> VPQ;
 
-    VPQ.push({SV, 0});
+    VPQ.push({0.0, SV});
     VM[SV] = nullptr;
-    CM[SV] = 0;
+    CM[SV] = 0.0;
 
     while (!VPQ.empty()) {
-        auto AV = VPQ.top().first;
+        auto AV = VPQ.top().second;
+        VPQ.pop();
+
         if (AV == EV) {
-            std::vector<common::Node*> path;
+            std::vector<common::Node*> VL;
             for (auto* node = EV; node != nullptr; node = VM[node]) {
-                path.push_back(node);
+                VL.push_back(node);
             }
-            std::reverse(path.begin(), path.end());
-            return path;
+            std::reverse(VL.begin(), VL.end());
+
+            if (VL.size() <= 2) return VL;
+
+            std::vector<common::Node*> PL;
+            PL.push_back(VL[0]);
+
+            for (size_t i = 1; i < VL.size() - 1; ++i) {
+                common::Node* P1 = PL.back();
+                common::Node* P2 = VL[i];
+                common::Node* P3 = VL[i+1];
+
+                auto [x1, y1] = std::any_cast<std::tuple<int, int>>(P1->getData());
+                auto [x2, y2] = std::any_cast<std::tuple<int, int>>(P2->getData());
+                auto [x3, y3] = std::any_cast<std::tuple<int, int>>(P3->getData());
+
+                long long crossProduct = 1LL * (x2 - x1) * (y3 - y2) - 1LL * (y2 - y1) * (x3 - x2);
+                bool isCollinear = (crossProduct == 0);
+
+                if (!isCollinear) {
+                    PL.push_back(P2);
+                }
+            }
+            
+            PL.push_back(VL.back());
+            return PL; 
         }
+
         auto NV = AV->adj();
         for (auto* neighbor : NV) {
-            auto VC = W[SV->getEdgeTo(neighbor)];
+            auto VC = CM[AV] + W[AV->getEdgeTo(neighbor)];
+            
             if (CM.find(neighbor) == CM.end() || VC < CM[neighbor]) {
                 CM[neighbor] = VC;
-                auto M = W[neighbor->getEdgeTo(AV)];
-                auto NVP = CM[neighbor] + heuristic(neighbor, EV) * M;
-                CM[neighbor] = NVP;
-                VPQ.push({neighbor, NVP});
+                
+                auto [ax, ay] = std::any_cast<std::tuple<int, int>>(AV->getData());
+                auto [nx, ny] = std::any_cast<std::tuple<int, int>>(neighbor->getData());
+                bool isDiagonal = (ax != nx) && (ay != ny);
+                
+                double M = isDiagonal ? std::sqrt(2.0) : 1.0; 
+                auto NVP = VC + heuristic(neighbor, EV) * M;
+                
+                VPQ.push({NVP, neighbor});
                 VM[neighbor] = AV;
             }
         }
     }
+    
     return {};
-};
+}
