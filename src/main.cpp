e7d1c586b1b8e1effee2cb6b49a9185586c2b646
andrevbastos <andrevbm.288@gmail.com>
2026-04-01 19:06:55 -0300

    why not working? will fix other day

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,18 @@ const int imageSize = 32;
 
 void warmUp();
 
+double euclidean3D(common::Node* a, common::Node* b) 
+{
+    try {
+        auto [xA, yA, zA] = std::any_cast<std::tuple<int, int, int>>(a->getData());
+        auto [xB, yB, zB] = std::any_cast<std::tuple<int, int, int>>(b->getData());
+        return std::sqrt(std::pow(xA - xB, 2) + std::pow(yA - yB, 2) + std::pow(zA - zB, 2));
+    } catch (const std::bad_any_cast& e) {
+        std::cerr << "Erro ao calcular a heurística: " << e.what() << std::endl;
+        return 0.0;
+    }
+};
+
 int main(int argc, char* argv[]) {
     srand(static_cast<unsigned>(time(NULL)));
 
@@ -47,7 +59,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    auto a_star_path = util::AStar::getPath(g, startId, endId, util::AStar::chebyshevHeuristic3D);
+    auto a_star_path = util::AStar::getPath(g, startId, endId, euclidean3D);
+    // auto a_star_path = aStarMod(g, startId, endId, util::AStar::chebyshevHeuristic3D);
 
     auto* floor = createMeshFromGraph(g, 0.5f, 0.5f, 0.5f, 1.0f, shader, GL_TRIANGLES);
     
